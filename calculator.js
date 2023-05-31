ea9cc0c219af9011ea20d1c04761959c59b0b781
Lone <Lone0059@gmail.com>
2023-05-31 16:45:05 +0500

    Calculator With 4 Operations
    This commit addes not only 4 operations but also feature of clearing the display of the screen after equally.

--- calculator.js
+++ calculator.js
@@ -1,17 +1,17 @@
 function add(a,b){
-    console.log(a+b);
+    return parseInt(a) + parseInt(b);
 };
 
 function subtract(a,b){
-    console.log(a-b);
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a,b){
-    console.log(a*b);
+    return parseInt(a) * parseInt(b);
 }
 
 function divide(a,b){
-    console.log(a/b);
+    return parseInt(a) / parseInt(b);
 }
 
 
@@ -24,7 +24,21 @@ let operator = 0;// will be the operator
 
 
 function operate(operator=0, num1, num2){
-    console.log(add(num1,num2));
+    switch(operator){
+        case "+":
+            return add(num1,num2);
+            break;
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1,num2);
+        default:
+            console.log(`ERROR OCCURRED`);
+    }
+
+
 }
 
 
@@ -32,5 +46,49 @@ function operate(operator=0, num1, num2){
 const calculator = document.getElementById("calculator");
 
 calculator.addEventListener('click', (e)=>{
+    
+    
     display_value = e.target.className;
+    let result = "";
+    // Display on screen of calculator
+    const screen = calculator.querySelector(".display")
+    
+    if(!(display_value.includes('clear')) && !(display_value.includes('container')) && !(display_value.includes('equal')) && !(display_value.includes('display'))){
+        screen.value += display_value;
+    };
+    
+    // Handle Display (Number // Operator // Number...)
+    if (e.target.className == 'equal'){
+        // Trigger the sort_value
+        num1,num2,operator = sortValue(screen.value);
+        result = operate(operator,num1,num2);
+        // Handle Display Result
+        if(!(result == 'ERROR OCCURRED')){
+            screen.value = result;
+        }
+    };
+    // Listen for clear button as well
+    if(e.target.className == 'clear'){
+        clearDisplay(screen);
+    }
+
 });
+
+
+function clearDisplay(screen){
+    screen.value = "";
+}
+
+function sortValue(value){
+    let index = "";
+    // First Number
+    ["+","*","/","-"].forEach((oper)=>{
+       if(value.includes(oper)){
+        index = value.indexOf(oper);
+       }
+    });
+    num1 = value.slice(0,index);
+    num2 = value.slice(index+1);
+    operator = value[index];
+    return num1,num2,operator
+};
